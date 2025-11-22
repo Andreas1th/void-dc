003d7ee7e9b2398d6c2c04fa198f93a2eba4b2f8
Andreas1th <alxn.rillcuy@gmail.com>
2025-11-23 01:53:47 +0700

    Create 8ball.js

---
+++ commands/fun/8ball.js
@@ -0,0 +1,52 @@
+import { SlashCommandBuilder, EmbedBuilder } from 'discord.js';
+
+export const fun8ballCommand = {
+  data: new SlashCommandBuilder()
+    .setName('8ball')
+    .setDescription('Ask the magic 8-ball a question')
+    .addStringOption(option =>
+      option.setName('question')
+        .setDescription('Your question for the 8-ball')
+        .setRequired(true)),
+
+  async execute(interaction, bot) {
+    const question = interaction.options.getString('question');
+
+    const responses = [
+      'It is certain',
+      'It is decidedly so',
+      'Without a doubt',
+      'Yes definitely',
+      'You may rely on it',
+      'As I see it, yes',
+      'Most likely',
+      'Outlook good',
+      'Yes',
+      'Signs point to yes',
+      'Reply hazy, try again',
+      'Ask again later',
+      'Better not tell you now',
+      'Cannot predict now',
+      'Concentrate and ask again',
+      'Don\'t count on it',
+      'My reply is no',
+      'My sources say no',
+      'Outlook not so good',
+      'Very doubtful'
+    ];
+
+    const randomResponse = responses[Math.floor(Math.random() * responses.length)];
+
+    const embed = new EmbedBuilder()
+      .setColor('#9b59b6')
+      .setTitle('🎱 Magic 8-Ball')
+      .addFields(
+        { name: '❓ Question', value: question },
+        { name: '🔮 Answer', value: `**${randomResponse}**` }
+      )
+      .setFooter({ text: `Asked by ${interaction.user.tag}` })
+      .setTimestamp();
+
+    await interaction.reply({ embeds: [embed] });
+  }
+};
